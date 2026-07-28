2f28a1ab84a5cd583ca77ae53891b4d9a8fce5de
pavankalyan4444 <pkalyan7432@gmail.com>
2026-07-28 14:34:54 +0530

    Add files via upload

---
+++ MethodEx.java
@@ -0,0 +1,13 @@
+class MethodEx{
+
+   public void Display(String name,String rollno) {
+      System.out.println(name);
+      System.out.println(rollno);
+}
+ 
+  public static void main(String args[]) {
+     MethodEx m1=new MethodEx();
+  m1.Display("Pavan Kalyan","D2");
+}
+}
+  
